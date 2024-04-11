81fcd66bce3667976b652e1b1307af4bea2e3a9a
rhagelaar <ricohagelaar@gmail.com>
2024-04-11 13:58:17 +0200

    Adjusted fingerprinting to always return the same number of SNPs for fingerprinting

--- modules/local/fingerprint/heatmap/bin/fingerprintheatmap.R
+++ modules/local/fingerprint/heatmap/bin/fingerprintheatmap.R
@@ -6,25 +6,16 @@ options(stringsAsFactors = FALSE)
 
 ### Load libraries 
 library(VariantAnnotation)
+library(dplyr)
 library(pheatmap)
 
 # get command arguments
 args = commandArgs(trailingOnly = TRUE)
 inputVCFs = args[1]
 prefix = args[2]
-#inputVCFs = "~/hpc/pmc_vanboxtel/tools/ToolsVanBox_DEV/ASAP_oatv/demo/output/work/ce/9f9d638fe13b724c26eee180a44f37/PMCAHH1-MSH2KO-MSH2-C25A04SC15C05_dedup.gatk_unifiedgenotyper.vcf.gz, ~/hpc/pmc_vanboxtel/tools/ToolsVanBox_DEV/ASAP_oatv/demo/output/work/b8/c6eb480fa9aff31fbbd5fb48cc7777/PMCAHH1-MSH2KO-MSH2-C25A04_dedup.gatk_unifiedgenotyper.vcf.gz"
-#inputDir = "~/hpc/pmc_vanboxtel/projects/_external_projects/GenomeTox/3_Output/FingerPrint/"
 
 # Read in data
 fingerprint_files <- strsplit(inputVCFs, ",")[[1]]
-print(fingerprint_files)
-#test <- readVcf(fingerprint_files[1])
-
-
-
-
-#fingerprint_files <- list.files(path=inputDir, pattern=".vcf$", full.names = T, recursive = T)
-#fingerprint_files <- fingerprint_files[1:4]
 vcfs = lapply(fingerprint_files, function(file) readVcf(file))
 
 # Function to convert the genotyping in solid values
@@ -36,10 +27,11 @@ convert_GT <- function( vcf ) {
 
 # Visualize the fingerprint data 
 df <- as.data.frame(lapply(vcfs, function(x) convert_GT(x)))
-pdf(paste0(prefix, "_fingerprintheatmap.pdf"))
-pheatmap(as.matrix(sapply(df, as.numeric)), fontsize_col = 6)
+df <- df %>% replace(is.na(.), 0)
+pdfwidth = 1.5*ncol(df)
+pdf(paste0(prefix, "_fingerprintheatmap.pdf"), height = 10, width = pdfwidth)
+pheatmap(as.matrix(sapply(df, as.numeric)), fontsize_col = 6, cellwidth = 6, cellheight = 6)
 dev.off()
-# lapply(paste('package:',names(sessionInfo()$otherPkgs),sep=""),detach,character.only=TRUE,unload=TRUE)
 
 
 
